b25272561ed4e8e4e8f07c76ed76aa6c5526228c
sumeet <maheshwarisumeet128@gmail.com>
2023-11-21 10:10:55 +0530

    main.py pushed

---
+++ app.py
@@ -0,0 +1,18 @@
+import gradio as gr
+from utils.helper import TextToSpeech
+
+def convert_text_to_speech(text):
+    audio_filename = "Output.mp3"
+    text_to_speech = TextToSpeech(text=text, filename= audio_filename)
+
+    if text_to_speech.convert():
+        return "Text is converted to Speech Sucessfully"
+    else:
+        return "An Error Occured During conversion"
+    
+iface = gr.inferface(
+    fn = convert_text_to_speech,
+    input ="text",
+    output = 'text',
+)
+iface.launch()
